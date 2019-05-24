d75711a3b0738d74837d8c6538c3f22aa9539c31
Shy Agam <us@bespunky.io>
2019-05-25 01:14:44 +0300

    :bug: Added missing exports

--- projects/angular-zen/src/public-api.ts
+++ projects/angular-zen/src/public-api.ts
@@ -3,3 +3,12 @@
  */
 
 export * from './lib/angular-zen.module';
+
+export * from './lib/core/core.module';
+export * from './lib/core/WindowRef/window-ref.service';
+export * from './lib/core/DocumentRef/document-ref.service';
+
+export * from './lib/loader/loader.module';
+export * from './lib/loader/LazyScriptLoader/lazy-script-loader.service';
+export * from './lib/loader/LazyScriptLoader/script-load-options';
+export * from './lib/loader/LazyScriptLoader/lazy-loaded-script';
