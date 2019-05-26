215f59ad7b154185959eab1c6d0d4ab06a28fcec
Shy Agam <us@bespunky.io>
2019-05-26 21:07:41 +0300

    :white_check_mark: Fixed e2e test

--- projects/demo-e2e/src/app.e2e-spec.ts
+++ projects/demo-e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display the title', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to demo!');
+    expect(page.getTitleText()).toEqual('angular-zen Demo');
   });
 
   afterEach(async () => {
--- projects/demo-e2e/src/app.po.ts
+++ projects/demo-e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText() {
-    return element(by.css('zen-root h1')).getText() as Promise<string>;
+    return element(by.css('h1')).getText() as Promise<string>;
   }
 }
