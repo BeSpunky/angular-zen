ec5ee391a8d34115b36299313743e37efa4ba141
Shy Agam <us@bespunky.io>
2019-05-28 20:33:29 +0300

    :art: Improved demo app structure and added styles

--- projects/demo/src/app/modules/loader-demo/loader-demo.component.ts
+++ projects/demo/src/app/modules/loader-demo/loader-demo.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { LazyScriptLoaderService, ScriptLoadOptions } from 'angular-zen';
 
 @Component({
     selector: 'zen-loader-demo',
@@ -8,28 +7,9 @@ import { LazyScriptLoaderService, ScriptLoadOptions } from 'angular-zen';
 })
 export class LoaderDemoComponent implements OnInit
 {
-    public status: string;
-
-    constructor(private loader: LazyScriptLoaderService) { }
+    constructor() { }
 
     ngOnInit()
     {
-        this.load(true);
-    }
-
-    public load(overrideCheck: boolean)
-    {
-        const options: ScriptLoadOptions = {};
-
-        if (overrideCheck) options.alreadyLoaded = () => (window as any).$;
-
-        this.status = 'Loading jQuery script async...';
-
-        this.loader.loadScript('https://code.jquery.com/jquery-3.4.1.min.js', options)
-            .subscribe(lazyScript =>
-            {
-                this.status = 'jQuery script loaded. Check <head> element.';
-                console.log(lazyScript.url, lazyScript.completed);
-            });
     }
 }
