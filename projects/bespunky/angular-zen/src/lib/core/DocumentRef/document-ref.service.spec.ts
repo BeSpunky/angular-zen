5bdd32b21141016d55fc23d5e470b102223606e6
Shy Agam <us@bespunky.io>
2019-05-30 00:17:11 +0300

    :white_check_mark: Updated DocumentRef testing

--- projects/bespunky/angular-zen/src/lib/core/DocumentRef/document-ref.service.spec.ts
+++ projects/bespunky/angular-zen/src/lib/core/DocumentRef/document-ref.service.spec.ts
@@ -1,25 +1,93 @@
 import { TestBed } from '@angular/core/testing';
+import { PLATFORM_ID } from '@angular/core';
 
-import { DocumentRef } from './document-ref.service';
+import { DOCUMENT, DocumentRef, DefaultDocumentRefProvider } from './document-ref.service';
+
+// These are not exported by angular so they are redefined here
+const PLATFORM_BROWSER_ID = 'browser';
+const PLATFORM_SERVER_ID = 'server';
+const PLATFORM_WORKER_APP_ID = 'browserWorkerApp';
+const PLATFORM_WORKER_UI_ID = 'browserWorkerUi';
 
 describe('DocumentRef', () =>
 {
+    const urlStub = 'angular-zen demo';
+
+    let documentMock: any;
     let service: DocumentRef;
 
     beforeEach(() =>
     {
-        TestBed.configureTestingModule({});
-
-        service = TestBed.get(DocumentRef);
+        documentMock = {
+            URL: urlStub
+        };
     });
 
-    it('should be created', () =>
+    describe('basically', () =>
     {
-        expect(service).toBeTruthy();
+        beforeEach(() =>
+        {
+            TestBed.configureTestingModule({
+                providers: [
+                    DefaultDocumentRefProvider,
+                    { provide: DOCUMENT, useValue: documentMock }
+                ]
+            });
+
+            service = TestBed.get(DocumentRef);
+        });
+
+        it('should be created', () =>
+        {
+            expect(service).toBeTruthy();
+        });
+
+        it('should allow access to the `document` object', () =>
+        {
+            expect(typeof service.nativeDocument).toBe('object');
+        });
     });
 
-    it('should allow access to the `document` object', () =>
+    describe('running on browser platforms', () =>
     {
-        expect(typeof service.nativeDocument).toBe('object');
+        beforeEach(() =>
+        {
+            TestBed.configureTestingModule({
+                providers: [
+                    DefaultDocumentRefProvider,
+                    { provide: PLATFORM_ID, useValue: PLATFORM_BROWSER_ID },
+                    { provide: DOCUMENT, useValue: documentMock }
+                ]
+            });
+
+            service = TestBed.get(DocumentRef);
+        });
+
+        it('should return the `document` object', () =>
+        {
+            expect(service.nativeDocument).toEqual(documentMock);
+        });
+    });
+
+    describe('running on non-browser platforms', () =>
+    {
+        beforeEach(() =>
+        {
+            TestBed.configureTestingModule({
+                providers: [
+                    DefaultDocumentRefProvider,
+                    { provide: PLATFORM_ID, useValue: PLATFORM_SERVER_ID },
+                    { provide: DOCUMENT, useValue: documentMock }
+                ]
+            });
+
+            service = TestBed.get(DocumentRef);
+        });
+
+        it('should return an empty object', () =>
+        {
+            expect(service.nativeDocument).not.toEqual(documentMock);
+            expect(service.nativeDocument).not.toContain('URL');
+        });
     });
 });
