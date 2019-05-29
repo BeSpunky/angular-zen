9cba02d508484765faa121eeb0857b8eb2e7e3b4
Shy Agam <us@bespunky.io>
2019-05-30 00:16:44 +0300

    :art: Improved DocumentRef testability

--- projects/bespunky/angular-zen/src/lib/core/DocumentRef/document-ref.service.ts
+++ projects/bespunky/angular-zen/src/lib/core/DocumentRef/document-ref.service.ts
@@ -1,15 +1,78 @@
-import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
+import { Injectable, Inject, InjectionToken, PLATFORM_ID, ValueProvider, ClassProvider } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
+/**
+ * An injectable token that will allow us to replace the provider for the native document object when necessary (e.g. mocking the `document` object).
+ */
+export const DOCUMENT = new InjectionToken<any>('DocumentToken');
+
+/**
+ * Provides an injectable wrapper for the `document` object.
+ *
+ * Inject this in your services/components and you will be able to easily mock or spy on the native `document` object in your tests.
+ * You can:
+ * - Replace the default `DOCUMENT` token provider, which allows you to mock the `document` object.
+ * - Replace the default `DocumentRef` service provider, which allows you to replace the logic for retrieving the `document` object.
+ *
+ * The default implementation provider for this service is defined by `DefaultDocumentRefProvider`.
+ * @see document-ref.service.spec.ts for examples.
+ * @export
+ */
 @Injectable({
     providedIn: 'root'
 })
 export class DocumentRef
 {
-    constructor(@Inject(PLATFORM_ID) private readonly platformId: any) { }
+    constructor() { }
 
     get nativeDocument(): any
     {
-        return isPlatformBrowser(this.platformId) ? document : new Object();
+        throw new Error('Not implemented. Either use `DefaultDocumentRefProvider` or extend `DocumentRef` and provide your own implementation.');
     }
 }
+
+/**
+ * Provides a platform dependant implementation for retrieving the native `document` object.
+ * For browser platforms, `nativeDocument` will return the native `document` object.
+ * For non-browser platforms, `nativeDocument` will return an empty object.
+ *
+ * This is the default implmentation provider for the `DocumentRef` class.
+ * @see `DefaultDocumentRefProvider`
+ *
+ * @export
+ */
+@Injectable({
+    providedIn: 'root'
+})
+export class PlatformDependantDocumentRef extends DocumentRef
+{
+    constructor(@Inject(PLATFORM_ID) private platformId: any, @Inject(DOCUMENT) private document: any)
+    {
+        super();
+    }
+
+    get nativeDocument(): any
+    {
+        return isPlatformBrowser(this.platformId) ? this.document : new Object();
+    }
+}
+
+/**
+ * The default provider for the `DOCUMENT` token.
+ * By default, provides the native `document` object.
+ */
+export const DefaultDocumentProvider: ValueProvider = {
+    provide: DOCUMENT,
+    useValue: document
+};
+
+/**
+ * The default `DocumentRef` provider.
+ * By default provides the platform dependant implementation.
+ */
+export const DefaultDocumentRefProvider: ClassProvider = {
+    provide: DocumentRef,
+    useClass: PlatformDependantDocumentRef
+};
+
+export const DocumentRefProviders = [DefaultDocumentProvider, DefaultDocumentRefProvider];
