89d2b45840626670b1215abf41e0858d4cd81b9b
Shy Agam <us@bespunky.io>
2019-10-31 13:43:00 +0200

    ♻ Extracted magic css url to an exported const

--- projects/demo/src/app/modules/async-demo/lazy-style-loader-demo/lazy-style-loader-demo.component.ts
+++ projects/demo/src/app/modules/async-demo/lazy-style-loader-demo/lazy-style-loader-demo.component.ts
@@ -1,25 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { LazyLoaderService } from '@bespunky/angular-zen';
+
+export const MagicStyleUrl = '/assets/magic.css';
 
 @Component({
     selector: 'zen-lazy-style-loader-demo',
     templateUrl: './lazy-style-loader-demo.component.html',
     styleUrls: ['./lazy-style-loader-demo.component.css']
 })
-export class LazyStyleLoaderDemoComponent implements OnInit
+export class LazyStyleLoaderDemoComponent
 {
     public loading = false;
 
     constructor(private loader: LazyLoaderService) { }
 
-    ngOnInit()
-    {
-    }
-
     public loadStyle()
     {
         this.loading = true;
 
-        this.loader.loadStyle('/assets/magic.css').subscribe(_ => this.loading = false);
+        this.loader.loadStyle(MagicStyleUrl).subscribe(_ => this.loading = false);
     }
 }
