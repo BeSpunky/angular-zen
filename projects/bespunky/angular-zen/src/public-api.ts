17b0df66cda6c54e01ba5d1064e65229615df9fe
Shy Agam <us@bespunky.io>
2020-04-18 14:42:56 +0300

    ♻ Removed the source folder and flattened the main public api

--- projects/bespunky/angular-zen/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-zen
- */
-
-export * from '@bespunky/angular-zen/core';
-export * from '@bespunky/angular-zen/async';
-export * from '@bespunky/angular-zen/universal';
