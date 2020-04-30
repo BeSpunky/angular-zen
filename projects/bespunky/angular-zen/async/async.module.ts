a92676585fca8a1769fe57c41514596cce1229f0
Shy Agam <us@bespunky.io>
2020-04-30 13:41:15 +0300

    ♻ Replaced angular platform detection code with universal service

--- projects/bespunky/angular-zen/async/async.module.ts
+++ projects/bespunky/angular-zen/async/async.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 
 import { CoreModule } from '@bespunky/angular-zen/core';
+import { UniversalModule } from '@bespunky/angular-zen/universal';
 
 @NgModule({
     declarations: [],
-    imports: [ CoreModule ],
+    imports: [ CoreModule, UniversalModule ],
     exports: [ CoreModule ]
 })
 export class AsyncModule { }
