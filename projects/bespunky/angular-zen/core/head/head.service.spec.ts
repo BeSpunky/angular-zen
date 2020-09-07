68bf4c4fe57206cc2f64fa8a5d78a027c5e1c137
Shy Agam <us@bespunky.io>
2020-09-07 21:37:56 +0300

    ✅ Added tests for addScriptElement in head service

--- projects/bespunky/angular-zen/core/head/head.service.spec.ts
+++ projects/bespunky/angular-zen/core/head/head.service.spec.ts
@@ -1,16 +1,62 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { HeadService } from './head.service';
+import { setupDocumentRefMock, MockScriptTag, MockLinkTag } from '@bespunky/angular-zen/core/testing';
+import { ScriptConfigurator                               } from '@bespunky/angular-zen/core';
+import { HeadService                                      } from './head.service';
+import { DOCUMENT                                         } from '../document-ref/document-ref.service';
 
-// describe('HeadService', () => {
-//   let service: HeadService;
+describe('HeadService', () =>
+{
+    let service: HeadService;
+    // Mock for the DocumentRef.nativeDocument object
+    let mockDocument: any;
+    // Mock for the document.head object
+    let mockHead: any;
+    // Stub for the instance of the script tag that will be created when calling loadScript()
+    let mockScriptTag: MockScriptTag;
+    // Stub for the instance of the link tag that will be created when calling loadStyle()
+    let mockLinkTag: MockLinkTag;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(HeadService);
-//   });
+    beforeEach(() =>
+    {
+        ({ mockScriptTag, mockLinkTag, mockHead, mockDocument } = setupDocumentRefMock());
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+        TestBed.configureTestingModule({
+            providers: [{ provide: DOCUMENT, useValue: mockDocument }]
+        });
+        
+        service = TestBed.inject(HeadService);
+    });
+
+    it('should be created', () => expect(service).toBeTruthy());
+
+    describe('calling `addScriptElement()`', () =>
+    {
+        function testScript(config?: ScriptConfigurator, expectMore?: () => void)
+        {
+            const type = 'application/javascript';
+            const src = 'https://dummy.url';
+            
+            service.addScriptElement(type, src, config);
+
+            expect(mockHead.appendChild).toHaveBeenCalledWith(mockScriptTag);
+            expect(mockScriptTag.type).toBe(type);
+            expect(mockScriptTag.src).toBe(src);
+
+            if (expectMore) expectMore();
+        }
+
+        it('should create a <script> element in <head> when calling `addScriptElement()` with no config', () => testScript());
+
+        it('should create a <script> element in <head> when calling `addScriptElement()` a config object', () => testScript({ async: true, onload: () => void 0 }, () =>
+        {
+            expect(mockScriptTag.async).toBeTruthy();
+            expect(mockScriptTag.onload instanceof Function).toBeTruthy();
+        }));
+
+        it('should create a <script> element in <head> when calling `addScriptElement()` a config function', () => testScript(script => script.onerror = () => void 0, () =>
+        {
+            expect(mockScriptTag.onerror instanceof Function).toBeTruthy();
+        }));
+    });
+});
