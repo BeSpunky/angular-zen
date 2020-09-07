5cc9ff4be588053e3eb9c0c8aaef4ffddbad8115
Shy Agam <us@bespunky.io>
2020-09-07 19:21:16 +0300

    ➕ Implemented a head service to handle elements in <head>

---
+++ projects/bespunky/angular-zen/core/head/head.service.spec.ts
@@ -0,0 +1,16 @@
+// import { TestBed } from '@angular/core/testing';
+
+// import { HeadService } from './head.service';
+
+// describe('HeadService', () => {
+//   let service: HeadService;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(HeadService);
+//   });
+
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
--- projects/bespunky/angular-zen/core/public-api.ts
+++ projects/bespunky/angular-zen/core/public-api.ts
@@ -3,3 +3,4 @@ export * from './core.module';
 
 export * from './window-ref/window-ref.service';
 export * from './document-ref/document-ref.service';
+export * from './head/head.service';
