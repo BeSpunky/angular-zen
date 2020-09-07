3478936cab67fe5edd6098072cf2ba4c2e415fe4
Shy Agam <us@bespunky.io>
2020-09-07 19:24:51 +0300

    ✅ Fixed undone specs for promiseLater

--- projects/bespunky/angular-zen/async/utils/utils.spec.ts
+++ projects/bespunky/angular-zen/async/utils/utils.spec.ts
@@ -11,20 +11,27 @@ describe('promiseLater()', () =>
         expect(later.reject instanceof Function).toBeTruthy();
     });
 
-    it('should return a `resolve()` method that acts on the returned promise', () =>
+    it('should return a `resolve()` method that acts on the returned promise', (done: DoneFn) =>
     {
         const later = promiseLater();
 
-        later.promise.then((value) => expect(value).toBe('resolved'));
+        later.promise.then((value) =>
+        {
+            expect(value).toBe('resolved');
+            done();
+        });
 
         later.resolve('resolved');
     });
 
-    it('should return a `reject()` method that acts on the returned promise', () =>
+    it('should return a `reject()` method that acts on the returned promise', (done: DoneFn) =>
     {
         const later = promiseLater();
 
-        later.promise.then(() => void 0, (reason) => expect(reason).toBe('rejected'));
+        later.promise.then(() => void 0, (reason) => {
+            expect(reason).toBe('rejected');
+            done();
+        });
 
         later.reject('rejected');
     });
