723d6c41d3094751c6027fab8b60288cc59e5765
Shy Agam <us@bespunky.io>
2020-09-27 02:32:12 +0300

    ➖ Removed deprecated document provider from core module

--- projects/bespunky/angular-zen/core/core.module.ts
+++ projects/bespunky/angular-zen/core/core.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
-import { WindowRefProviders   } from './window-ref/window-ref.service';
-import { DocumentRefProviders } from './document-ref/document-ref.service';
+import { NgModule           } from '@angular/core';
+import { WindowRefProviders } from './window-ref/window-ref.service';
 
 @NgModule({
     declarations: [],
     imports: [],
     providers: [
-        WindowRefProviders,
-        // DocumentRefProviders
+        WindowRefProviders
     ],
     exports: []
 })
