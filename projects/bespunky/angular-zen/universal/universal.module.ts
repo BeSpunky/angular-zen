62e82eb03ceadd284b05a8745e7efbce9f8dfa97
Shy Agam <us@bespunky.io>
2020-11-02 12:46:17 +0200

    🎨 Code layout

--- projects/bespunky/angular-zen/universal/universal.module.ts
+++ projects/bespunky/angular-zen/universal/universal.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserOnlyDirective } from './directives/browser-only.directive';
-import { ServerOnlyDirective } from './directives/server-only.directive';
-import { WorkerAppOnlyDirective } from './directives/worker-app-only.directive';
-import { WorkerUiOnlyDirective } from './directives/worker-ui-only.directive';
-import { NonBrowserOnlyDirective } from './directives/non-browser-only.directive';
-import { NonServerOnlyDirective } from './directives/non-server-only.directive';
+import { BrowserOnlyDirective      } from './directives/browser-only.directive';
+import { ServerOnlyDirective       } from './directives/server-only.directive';
+import { WorkerAppOnlyDirective    } from './directives/worker-app-only.directive';
+import { WorkerUiOnlyDirective     } from './directives/worker-ui-only.directive';
+import { NonBrowserOnlyDirective   } from './directives/non-browser-only.directive';
+import { NonServerOnlyDirective    } from './directives/non-server-only.directive';
 import { NonWorkerAppOnlyDirective } from './directives/non-worker-app-only.directive';
-import { NonWorkerUiOnlyDirective } from './directives/non-worker-ui-only.directive';
+import { NonWorkerUiOnlyDirective  } from './directives/non-worker-ui-only.directive';
 
 const exported = [
     BrowserOnlyDirective,
     ServerOnlyDirective,
     WorkerAppOnlyDirective,
     WorkerUiOnlyDirective,
+    
     NonBrowserOnlyDirective,
     NonServerOnlyDirective,
     NonWorkerAppOnlyDirective,
