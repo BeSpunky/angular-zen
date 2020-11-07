ba43d3cb2238343f72fa1438d5df75b0d6aaee11
Shy Agam <us@bespunky.io>
2020-11-07 19:20:34 +0200

    🐛 Fixed AOT compilation error
    Not sure what the reason is.
    Node 12 gives a stack overflow error.
    Node 14 also gave an error message related with circular dependencies but seems like it has to do with dependencies, not the library itself.
    
    Introduced fixed array as a workaround.

--- projects/bespunky/angular-zen/router-x/testing/utils/routes.ts
+++ projects/bespunky/angular-zen/router-x/testing/utils/routes.ts
@@ -4,7 +4,7 @@ export const DeepRoutePath           = '/deeply/nested/route/for/testing';
  * The segments of `DeepRoutePath`. First element will always be '' (empty string) as the route begins with a forward slash.
  * Use `DeepRouteSegmentsNoRoot` for an array of segments without the root ''.
  */
-export const DeepRouteSegments       = DeepRoutePath.split('/');
+export const DeepRouteSegments       = ['', 'deeply', 'nested', 'route', 'for', 'testing']; // For some reasone `DeepRoutePath.split('/')` causes a stack overflow when building with --prod.
 /**
  * The segments of `DeepRoutePath` without the root route (the '' route).
  * Use `DeepRouteSegments` for an array of segments with the root ''.
