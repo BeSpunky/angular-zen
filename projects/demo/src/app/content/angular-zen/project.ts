186ecccd8b92f2384f3ca79c871ed561d8fff05c
Shy Agam <us@bespunky.io>
2020-11-08 22:55:24 +0200

    ✍ Updated zen benefits

--- projects/demo/src/app/content/angular-zen/project.ts
+++ projects/demo/src/app/content/angular-zen/project.ts
@@ -2,10 +2,14 @@ import { Project   } from '../../types/project';
 import { ZenTopics } from './examples/all';
 
 export const ZenBenefits = [
-    { title: 'Plug & Play',             emoji: '🔌', description: 'Easy to use. The intuitive library structure and its API provide for maximum power with minimum effort and learning curve.' },
-    { title: 'Tree-Shakable',           emoji: '🌳', description: 'Any library module you don\'t use in your app will be excluded from your final bundle.' },
-    { title: 'Angular Universal Tools', emoji: '🌎', description: 'Services and directives facilitate with SSR.' },
-    { title: 'Open-source!',            emoji: '🤩', description: 'Learn how it works! Contribute! Fork it! Make it your own!' }
+    { title: 'Plug & Play',                emoji: '🔌', description: 'Easy to use. The intuitive library structure and its API provide for maximum power with minimum effort and learning curve.' },
+    { title: 'Tree-Shakable',              emoji: '🌳', description: 'Any library module you don\'t use in your app will be excluded from your final bundle.' },
+    { title: 'Fastar Scaffolding',         emoji: '🏗', description: 'Create services and components quicker.' },
+    { title: 'Worry Less',                 emoji: '🥳', description: 'Well tested infrustructural tools give you deeper control with no need to dive deep yourself.' },
+    { title: 'Angular Universal Tools',    emoji: '🌎', description: 'Services and directives facilitate with SSR.' },
+    { title: 'Router Extensions',          emoji: '🔀', description: 'Improved routing tools experience.' },
+    { title: 'Language Integration Tools', emoji: '🗣', description: 'Quickly add and standardize language support to your libraries.' },
+    { title: 'Open-source!',               emoji: '🤩', description: 'Learn how it works! Contribute! Fork it! Make it your own!' }
 ];
 
 export const ZenCredits = [
@@ -15,7 +19,7 @@ export const ZenCredits = [
 export const ZenProject: Project = {
     name          : 'angular-zen',
     strongName    : 'Angular Zen',
-    slogan        : 'Your default toolbox for Angular.',
+    slogan        : 'The Angular tools you always wished were there.',
     benefitsSlogan: 'Helps you focus on what you really need to do.',
     benefits      : ZenBenefits,
     examplesTopics: ZenTopics,
