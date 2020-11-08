bc2723a888f3a36466aca20dceee86469d152682
Shy Agam <us@bespunky.io>
2020-11-08 21:09:54 +0200

    🎨 Code layout

--- projects/demo/src/app/content/all.ts
+++ projects/demo/src/app/content/all.ts
@@ -1,7 +1,7 @@
-import { Project } from "../types/project";
-import { ZenProject } from "./angular-zen/project";
-import { ZenUxProject } from "./angular-zen-ux/project";
-import { ZenSeoProject } from "./angular-zen-seo/project";
+import { Project       } from '../types/project';
+import { ZenProject    } from './angular-zen/project';
+import { ZenUxProject  } from './angular-zen-ux/project';
+import { ZenSeoProject } from './angular-zen-seo/project';
 
 export const ZenProjects: Project[] = [
     ZenProject,
