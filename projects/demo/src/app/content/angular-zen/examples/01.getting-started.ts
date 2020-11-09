2e53b2d88ddee90bc0a4a8a874bf8ec4210ad48e
Shy Agam <us@bespunky.io>
2020-11-10 00:08:18 +0200

    ✍ Edited getting started content for zen project

--- projects/demo/src/app/content/angular-zen/examples/01.getting-started.ts
+++ projects/demo/src/app/content/angular-zen/examples/01.getting-started.ts
@@ -1,33 +1,40 @@
-// import { Example } from '../types/example';
-// import { Topic   } from '../types/topic';
+import { Example } from '../../../types/example';
+import { Topic   } from '../../../types/topic';
 
-// export const GettingStartedExamples: Example[] = [
-//     {
-//         title      : 'Plug & Play',
-//         description: 'Have the library import Google Maps API for you and add a simple map.',
-//         notes      : 'You\'ll need to have your own key to Google Maps API. See example for link.',
-//         icon       : 'power',
-//         embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-plug-n-play?embed=1&file=src/app/app.module.ts'
-//     },
-//     {
-//         title      : 'Custom Loader',
-//         description: 'Implement your own mechanism for loading Google Maps API.',
-//         notes      : 'You\'ll need to have your own key to Google Maps API. See example for link.',
-//         icon       : 'build',
-//         embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-custom-loader?embed=1&file=src/app/app.module.ts'
-//     },
-//     {
-//         title      : 'Manual Loading',
-//         description: 'Manually add a <script> tag to your <head> element to ensure maps API completely loads before the your app does.',
-//         notes      : 'You\'ll need to have your own key to Google Maps API. See example for link.',
-//         icon       : 'engineering',
-//         embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-manual-loading?embed=1&file=src/app/app.module.ts'
-//     }
-// ];
+export const GettingStartedExamples: Example[] = [
+    {
+        title      : 'WindowRef',
+        description: 'Safely access or mock the `window` object.',
+        icon       : 'desktop_windows',
+        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-plug-n-play?embed=1&file=src/app/app.module.ts',
+        soon: true
+    },
+    {
+        title      : 'DocumentRef',
+        description: 'Safely access or mock the `document` object.',
+        icon       : 'description',
+        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-custom-loader?embed=1&file=src/app/app.module.ts',
+        soon: true
+    },
+    {
+        title      : 'HeadService',
+        description: 'Work with the `<head>` element to create, modify, find and remove elements.',
+        icon       : 'face',
+        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-manual-loading?embed=1&file=src/app/app.module.ts',
+        soon: true
+    },
+    {
+        title      : 'Destroyable',
+        description: 'Subscribe to observables without worrying about memory leaks.',
+        icon       : 'local_fire_department',
+        embedUrl   : 'https://stackblitz.com/edit/bs-google-maps-manual-loading?embed=1&file=src/app/app.module.ts',
+        soon: true
+    }
+];
 
-// export const GettingStartedTopic: Topic = {
-//     title      : 'Getting Started',
-//     description: 'See how simple maps are integrated into an Angular app.',
-//     icon       : 'explore',
-//     examples   : GettingStartedExamples
-// };
+export const GettingStartedTopic: Topic = {
+    title      : 'Core Module',
+    description: 'Learn about the core infrastructural tools.',
+    icon       : 'memory',
+    examples   : GettingStartedExamples
+};
