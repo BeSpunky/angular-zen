a1e3de6d08ebf89ed6cb2b5ffb8d46a3b0f1a672
Shy Agam <us@bespunky.io>
2020-11-12 18:54:49 +0200

    🎨 Code layout

--- projects/demo/src/app/app-routing.module.ts
+++ projects/demo/src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {enableTracing: false})],
+  imports: [RouterModule.forRoot(routes, { enableTracing: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
