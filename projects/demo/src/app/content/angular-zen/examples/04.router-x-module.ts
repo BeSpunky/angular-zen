2042e5d5ea8117136272ed57d2bc67c4fbf03c9d
Shy Agam <us@bespunky.io>
2020-11-13 17:47:57 +0200

    ➕ Added router-x examples

--- projects/demo/src/app/content/angular-zen/examples/04.router-x-module.ts
+++ projects/demo/src/app/content/angular-zen/examples/04.router-x-module.ts
@@ -9,10 +9,22 @@ export const RouterXModuleExamples: Example[] = [
         embedUrl   : 'https://codesandbox.io/embed/router-outlet-component-bus-n32g0?expanddevtools=1&fontsize=14&hidenavigation=0&module=%2Fsrc%2Fapp%2Fdemo%2Fmonitor.service.ts&theme=dark'
     },
     {
-        title      : 'Route Awareness',
+        title      : 'Route Aware Event Handling',
         description: 'Creating route-aware components and services with ease.',
-        icon       : 'alt_route',
-        embedUrl   : 'https://codesandbox.io/embed/loading-scripts-vgf7t?fontsize=14&hidenavigation=1&module=%2Fsrc%2Fapp%2Fdemo%2Fdemo.component.ts&theme=dark', soon: true
+        icon       : 'flash_on',
+        embedUrl   : 'https://codesandbox.io/embed/router-events-rnvw5?expanddevtools=1&fontsize=14&hidenavigation=1&module=%2Fsrc%2Fapp%2Fdemo%2Fdemo.component.ts&theme=dark'
+    },
+    {
+        title      : 'Route Aware Deep Scan',
+        description: 'Performing a deep scan or accumulative actions on a route tree.',
+        icon       : 'search',
+        embedUrl   : 'https://codesandbox.io/embed/deep-route-scan-6i0uk?expanddevtools=1&fontsize=14&hidenavigation=1&module=%2Fsrc%2Fapp%2Fdemo%2Fdemo.component.ts&theme=dark'
+    },
+    {
+        title      : 'Route Aware Resolves',
+        description: 'Use route-aware tools to resolve async code at any time.',
+        icon       : 'sync_alt',
+        embedUrl   : 'https://codesandbox.io/embed/resolves-7e5iq?fontsize=14&hidenavigation=1&module=%2Fsrc%2Fapp%2Fdemo%2Fdemo.component.ts&theme=dark'
     },
     {
         title      : 'Url Reflection',
