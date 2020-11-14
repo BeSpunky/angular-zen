dc21e2d72ed33a52e60c32397c61e392b9af6244
Shy Agam <us@bespunky.io>
2020-11-14 21:56:49 +0200

    ➕ Organized wiki summary generation script and added ordering algorithm

--- generate-wiki-summary.js
+++ generate-wiki-summary.js
@@ -9,6 +9,31 @@ function log(message, level)
     console.log(`${prefix} ${message}`);
 }
 
+function extractFolderNamesFromContentList(contentList)
+{
+    return contentList.filter(item => !item.startsWith('.') && !item.endsWith('.md'));
+}
+
+function extractMarkdownDocsFromContentList(contentList)
+{
+    return contentList.filter(item => item.endsWith('.md'));
+}
+
+function createFolderMap(folders)
+{
+    return folders.reduce((folderMap, folderName) => ({ ...folderMap, [folderName]: folderName }), {});
+}
+
+// Creates a map of filename -> order index to easily pull and compare order indexes
+function createMarkdownOrderMap(path)
+{
+    const orderFile = `${path}/.order`;
+    const order     = fs.existsSync(orderFile) ? fs.readFileSync(orderFile, 'utf8').split('\r\n') : [];
+    
+    // Reduce to map object and add '.md' to filenames as that's how they will be received from the sort function
+    return order.reduce((map, fileName, index) => ({ ...map, [`${fileName}.md`]: index }), {});
+}
+
 function generateDirectorySummary(path, level)
 {
     path  = path  || '';
@@ -18,32 +43,34 @@ function generateDirectorySummary(path, level)
 
     const scannedPath = `${wikiFolder}/${path}`;
     const contentList = fs.readdirSync(scannedPath);
-    const folders     = contentList.filter(item => !item.startsWith('.') && !item.endsWith('.md'));
-    const docs        = contentList.filter(item => item.endsWith('.md'));
-    const folderMap   = folders.reduce((folderMap, folderName) => ({ ...folderMap, [folderName]: folderName }), {});
-    
+    const folders     = extractFolderNamesFromContentList(contentList);
+    const docs        = extractMarkdownDocsFromContentList(contentList);
+    const folderMap   = createFolderMap(folders);
+    const order       = createMarkdownOrderMap(scannedPath);
+
     log(`📃 Found ${docs.length} .md files.`, level);
 
-    return docs.map(doc =>
-    {
-        const fileName = doc.substring(0, doc.length - 3);
+    return docs.sort((doc1, doc2) => order[doc1] - order[doc2])
+               .map(doc =>
+                {
+                    const fileName = doc.substring(0, doc.length - 3);
 
-        const description = {
-            title: fileName.replace('-', ' '),
-            file : `${path}/${doc}`
-        };
+                    const description = {
+                        title: fileName.replace('-', ' '),
+                        file : `${path}/${doc}`
+                    };
 
-        if (folderMap[fileName])
-        {
-            log(`📂 Scanning '${fileName}' childern...`, level);
+                    if (folderMap[fileName])
+                    {
+                        log(`📂 Scanning '${fileName}' childern...`, level);
 
-            const childFolder = `${path}/${fileName}`;
+                        const childFolder = `${path}/${fileName}`;
 
-            description.children = generateDirectorySummary(childFolder, level + 1);
-        }
+                        description.children = generateDirectorySummary(childFolder, level + 1);
+                    }
 
-        return description;
-    });
+                    return description;
+                });
 }
 
 log('------------------------------------------');
