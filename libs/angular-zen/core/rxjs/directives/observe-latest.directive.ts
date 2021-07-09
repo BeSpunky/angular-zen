c06a15f3d3180c577fc80eb484a0b76940392ce9
Shy Agam <us@bespunky.io>
2021-07-09 11:37:13 +0300

    ➕ Implemented a working POC for template type narrowing
    The context variable is now correctly typed, but the directive itself doesn't enforce the correct input type.

--- libs/angular-zen/core/rxjs/directives/observe-latest.directive.ts
+++ libs/angular-zen/core/rxjs/directives/observe-latest.directive.ts
@@ -1,22 +1,27 @@
-import { Observable, combineLatest } from 'rxjs';
+import { Observable, combineLatest, EMPTY } from 'rxjs';
 import { Directive, Input          } from '@angular/core';
 
-import { ObserveMapDirective, Map, ObservableMapOf, observeAsArray, ObserveMapContext } from './abstraction/observe-map-base.directive';
+import { ObserveMapDirective, Map, ObservableMapOf, observeAsArray, ObserveMapContext, ObservableMap } from './abstraction/observe-map-base.directive';
+
+type ResolvedObservableMap<T> = {
+    [key in keyof T]: T[key] extends Observable<infer TResolved> ? TResolved : never
+};
 
 @Directive({
     selector: '[observeLatest]',
     // eslint-disable-next-line @angular-eslint/no-inputs-metadata-property
     // inputs: ['observe: observeLatest']
 })
-export class ObserveLatestDirective<T extends Map> extends ObserveMapDirective<T>
+export class ObserveLatestDirective<T = unknown> //extends ObserveMapDirective<T>
 {
-    // static ngTemplateGuard_observeLatest<T extends {[k: string]: Observable<any>}>(dir: ObserveLatestDirective<T>, expr: any): expr is {[k: string]: Observable<any>} { return true; };
-    static ngTemplateContextGuard<TT extends Map>(directive: ObserveLatestDirective<TT>, context: ObserveMapContext<TT>): context is ObserveMapContext<TT> { return true; }
+    @Input() public set observeLatest(value: T) { /* this.input.next(value) */ }
+    
+    static ngTemplateGuard_observeLatest: 'binding';
+    static ngTemplateContextGuard<T>(directive: ObserveLatestDirective<T>, context: unknown): context is ObserveMapContext<ResolvedObservableMap<T>> { return true; }
 
-    @Input() public set observeLatest(value: ObservableMapOf<T>) { this.input.next(value)}
-
-    protected observeInput(input: ObservableMapOf<T>): Observable<T>
+    protected observeInput(input: T): Observable<T>
     {
-        return observeAsArray(input, observables => combineLatest(observables));
+        // return observeAsArray(input, observables => combineLatest(observables));
+        return EMPTY;
     }
 }
