50a67b354ef13e71dbe3b80e6fc430ffb838bb99
Shy Agam <us@bespunky.io>
2021-07-12 20:16:10 +0300

    ➕ Implemented precision property and auto precision calculation in case not specified

--- libs/angular-zen/core/rxjs/when-observer/directives/when-observer.directive.ts
+++ libs/angular-zen/core/rxjs/when-observer/directives/when-observer.directive.ts
@@ -1,8 +1,8 @@
-import { Observable, Subject, of, iif, timer, EMPTY, interval, forkJoin           } from 'rxjs';
+import { Observable, Subject, of, timer, EMPTY, forkJoin, combineLatest           } from 'rxjs';
 import { delay, first, map, materialize, switchMap, takeWhile, tap                } from 'rxjs/operators';
 import { Directive, EmbeddedViewRef, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 
-import { Destroyable                                                          } from '../../destroyable/destroyable';
+import { Destroyable                                                            } from '../../destroyable/destroyable';
 import { ObserverHandler, DurationAnnotation, WhenObserverContext, DurationUnit } from '../abstraction/types/general';
 
 const StateNotificationMap: Record<'N' | 'E' | 'C', ObserverHandler> = {
@@ -13,6 +13,8 @@ const StateNotificationMap: Record<'N' | 'E' | 'C', ObserverHandler> = {
 
 const DurationMultipliers: Record<DurationUnit, number> = { ms: 1, s: 1000, m: 60000 };
 
+const DefaultDurationIntervalDivisor = 30;
+
 @Directive({
     // eslint-disable-next-line @angular-eslint/directive-selector
     selector: '[whenObserver]'
@@ -21,9 +23,10 @@ export class WhenObserverDirective<T> extends Destroyable implements OnInit
 {
     @Input() public set whenObserver(value: Observable<T>) { this.input.next(value); }
 
-    @Input() public whenObserverCalls    : ObserverHandler | ObserverHandler[] = 'next';
-    @Input() public whenObserverShowAfter: DurationAnnotation = 0;
-    @Input() public whenObserverShowFor? : DurationAnnotation;
+    @Input() public whenObserverCalls              : ObserverHandler | ObserverHandler[] = 'next';
+    @Input() public whenObserverShowAfter          : DurationAnnotation = 0;
+    @Input() public whenObserverShowFor?           : DurationAnnotation;
+    @Input() public whenObserverCountdownPrecision?: DurationAnnotation;
     
     private view    : EmbeddedViewRef<WhenObserverContext<unknown>> | null = null;
     private lastCall: ObserverHandler                                      = 'next';
@@ -40,6 +43,21 @@ export class WhenObserverDirective<T> extends Destroyable implements OnInit
         this.onHandlerCalled('next');
 
         this.subscribe(this.sourceFeed());
+    }
+
+    private defineCountdownPrecisionInterval(): number
+    {
+        // If the view should persist, or it should auto-destroy but percision has been manually specified, do nothing
+        if (!this.whenObserverShowFor) throw new Error(`
+            Auto-destroy countdown seems to have been initiated when 'showFor' hasn't been set. This shouldn't have happend.
+            Please consider filing an issue and providing a stack trace here: https://github.com/BeSpunky/angular-zen/issues/new?assignees=BeSpunky&labels=%F0%9F%90%9B+Bug&template=bug_report.md&title=%F0%9F%90%9B+
+        `);
+        
+        if (this.whenObserverCountdownPrecision) return this.durationToMs(this.whenObserverCountdownPrecision);
+
+        const showForMs = this.durationToMs(this.whenObserverShowFor);
+
+        return showForMs / DefaultDurationIntervalDivisor;
     }
 
     private sourceFeed(): Observable<unknown>
@@ -86,20 +104,25 @@ export class WhenObserverDirective<T> extends Destroyable implements OnInit
 
     private autoDestroy(): Observable<unknown>
     {
-        const destroyDelayMs = this.durationToMs(this.whenObserverShowFor ?? 0);
+        const showForMs            = this.durationToMs(this.whenObserverShowFor ?? 0);
+        const countdownPrecisionMs = this.defineCountdownPrecisionInterval();
 
-        const destroy = timer(destroyDelayMs).pipe(
+        const destroy = timer(showForMs).pipe(
             first(),
             tap(() => this.destroyView())
         );
 
-        const startTime = Date.now();
+        // Enclosing Date.now() in an observable to make sure it is called when the observable is
+        // actually subscribed to and not during the setup stage inside autoDestroy().
+        const startTime = of(0).pipe(
+            map(() => Date.now())
+        );
 
-        const keepingFor = interval(200).pipe(
-            map(() => Date.now() - startTime),
-            map(timePassedMs => destroyDelayMs - timePassedMs),
-            map(timeLeftMs => timeLeftMs < 0 ? 0 : timeLeftMs),
-            tap(timeLeftMs => this.updateContextShowFor(timeLeftMs)),
+        const keepingFor = combineLatest([startTime, timer(0, countdownPrecisionMs)]).pipe(
+            map(([startTime]) => Date.now() - startTime),
+            map(timePassedMs  => showForMs - timePassedMs),
+            map(timeLeftMs    => timeLeftMs < 0 ? 0 : timeLeftMs),
+            tap(timeLeftMs    => this.updateContextShowFor(timeLeftMs)),
             takeWhile(timeLeftMs => timeLeftMs > 0)
         );
 
@@ -118,8 +141,15 @@ export class WhenObserverDirective<T> extends Destroyable implements OnInit
 
     private updateContextShowFor(showingForMs: number): void
     {
-        if (this.view)
-            this.view.context.showingForMs = showingForMs;
+        if (!this.view) return;
+        
+        const dummyDate = new Date(showingForMs);
+
+        this.view.context.showingFor = {
+            m : dummyDate.getMinutes(),
+            s : dummyDate.getSeconds(),
+            ms: dummyDate.getMilliseconds()
+        };
     }
 
     private destroyView(): void
