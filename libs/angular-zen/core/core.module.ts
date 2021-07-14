df13b9b55316d510219a037a254f1549ff74395c
Shy Agam <us@bespunky.io>
2021-07-14 11:30:08 +0300

    ➕ Exposed new shortcut onObserver directives as public api

--- libs/angular-zen/core/core.module.ts
+++ libs/angular-zen/core/core.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 
-import { DocumentRefProviders   } from './document-ref/document-ref.service';
-import { WindowRefProviders     } from './window-ref/window-ref.service';
-import { ObserveDirective       } from './rxjs/observe/directives/observe.directive';
-import { ObserveLatestDirective } from './rxjs/observe/directives/observe-latest.directive';
-import { ObserveConcatDirective } from './rxjs/observe/directives/observe-concat.directive';
-import { ObserveJoinDirective   } from './rxjs/observe/directives/observe-join.directive';
-import { ObserveMergeDirective  } from './rxjs/observe/directives/observe-merge.directive';
-import { OnObserverDirective    } from './rxjs/on-observer/directives/on-observer.directive';
+import { DocumentRefProviders         } from './document-ref/document-ref.service';
+import { WindowRefProviders           } from './window-ref/window-ref.service';
+import { ObserveDirective             } from './rxjs/observe/directives/observe.directive';
+import { ObserveLatestDirective       } from './rxjs/observe/directives/observe-latest.directive';
+import { ObserveConcatDirective       } from './rxjs/observe/directives/observe-concat.directive';
+import { ObserveJoinDirective         } from './rxjs/observe/directives/observe-join.directive';
+import { ObserveMergeDirective        } from './rxjs/observe/directives/observe-merge.directive';
+import { OnObserverResolvingDirective } from './rxjs/on-observer/directives/on-observer-resolving.directive';
+import { OnObserverNextDirective      } from './rxjs/on-observer/directives/on-observer-next.directive';
+import { OnObserverErrorDirective     } from './rxjs/on-observer/directives/on-observer-error.directive';
+import { OnObserverCompleteDirective  } from './rxjs/on-observer/directives/on-observer-complete.directive';
+import { OnObserverActiveDirective    } from './rxjs/on-observer/directives/on-observer-active.directive';
+import { OnObserverFinalizedDirective } from './rxjs/on-observer/directives/on-observer-finalized.directive';
 
 @NgModule({
     providers: [
@@ -20,7 +25,12 @@ import { OnObserverDirective    } from './rxjs/on-observer/directives/on-observe
         ObserveJoinDirective,
         ObserveMergeDirective,
         ObserveConcatDirective,
-        OnObserverDirective,
+        OnObserverResolvingDirective,
+        OnObserverNextDirective,
+        OnObserverErrorDirective,
+        OnObserverCompleteDirective,
+        OnObserverActiveDirective,
+        OnObserverFinalizedDirective,
     ],
     exports: [
         ObserveDirective,
@@ -28,7 +38,12 @@ import { OnObserverDirective    } from './rxjs/on-observer/directives/on-observe
         ObserveJoinDirective,
         ObserveMergeDirective,
         ObserveConcatDirective,
-        OnObserverDirective,
+        OnObserverResolvingDirective,
+        OnObserverNextDirective,
+        OnObserverErrorDirective,
+        OnObserverCompleteDirective,
+        OnObserverActiveDirective,
+        OnObserverFinalizedDirective,
     ]
 })
 export class CoreModule { }
