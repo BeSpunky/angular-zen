d53e79ce9a25cf8f7a2c5bae87d33c47fe901efb
Shy Agam <us@bespunky.io>
2022-04-29 10:51:32 +0300

    ✨ Generated a playground app for development purposes
    This is where live testing

---
+++ apps/playground/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "bespunky-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"],
+})
+export class AppComponent {
+    title = "playground";
+}
