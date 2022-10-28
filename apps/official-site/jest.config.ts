a26bab8d9d8b7d4a3800fa141020da298d3785da
Shy Agam <us@bespunky.io>
2022-10-28 13:18:04 +0000

    🚢_Ran_migration_update-to-export-default

--- apps/official-site/jest.config.ts
+++ apps/official-site/jest.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-module.exports = {
+export default {
     displayName: "official-site",
     preset: "../../jest.preset.js",
     setupFilesAfterEnv: ["<rootDir>/src/test-setup.ts"],
--- libs/angular-zen/jest.config.ts
+++ libs/angular-zen/jest.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-module.exports = {
+export default {
     displayName: "angular-zen",
     preset: "../../jest.preset.js",
     setupFilesAfterEnv: ["<rootDir>/src/test-setup.ts"],
