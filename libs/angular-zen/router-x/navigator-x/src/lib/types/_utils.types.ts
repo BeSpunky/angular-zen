ff4b6804e85aff911ec934cea8197f4a64ba9c75
Shy Agam <us@bespunky.io>
2022-11-19 08:35:29 +0200

    🎨 Reorganized code

--- libs/angular-zen/router-x/navigator-x/src/lib/types/_utils.types.ts
+++ libs/angular-zen/router-x/navigator-x/src/lib/types/_utils.types.ts
@@ -1,9 +1,9 @@
 export type Primitive = undefined | null | number | bigint | boolean | string;
 
-export type Narrow<T> = T extends (...args: any[]) => any | Primitive | [] ? T : never |
-    {
-        [ K in keyof T ]: Narrow<T[ K ]>;
-    };
+export type Narrow<T> =
+    T extends Primitive | [] | ((...args: any[]) => any)
+    ? T
+    : { [ K in keyof T ]: Narrow<T[ K ]>; };
 
 export type AsObject<T> = {
     [ k in keyof T ]: T[ k ];
