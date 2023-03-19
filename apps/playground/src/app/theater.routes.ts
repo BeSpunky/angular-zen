e3651a4a285fe5937d2d06660a98c72ba2edc7a2
Shy Agam <us@bespunky.io>
2023-03-19 16:24:07 +0200

    🎨 Code layout

--- apps/playground/src/app/theater.routes.ts
+++ apps/playground/src/app/theater.routes.ts
@@ -2,7 +2,7 @@ import { routeConfigFor } from '@bespunky/angular-zen/router-x/navigation';
 import { TheaterShowComponent } from './theater-show/theater-show.component';
 import { TheaterShow } from './theater-show/model';
 
-const theater = routeConfigFor<TheaterShow>()
+const theater = routeConfigFor<TheaterShow>();
 
 export const theaterRoutes = theater.route({
     path: 'theaters',
