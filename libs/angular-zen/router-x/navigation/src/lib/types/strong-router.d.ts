d2dfea5910117c92df15a80811d702b192ada6bc
Shy Agam <us@bespunky.io>
2023-03-30 08:56:26 +0000

    🚫 Commented out temp or wip code

--- libs/angular-zen/router-x/navigation/src/lib/types/strong-router.d.ts
+++ libs/angular-zen/router-x/navigation/src/lib/types/strong-router.d.ts
@@ -1,12 +1,12 @@
-import type { RouteCommands } from './route-commands.types';
+// import type { RouteCommands } from './route-commands.types';
 
-declare module '@angular/router' {
-    interface Router
-    {
-        navigate<
-            Route extends
-            & ComposableRoute<ReadonlyRoute<string, string, any>>
-            & WithNavigationX<any, any, string>
-        >(url: RouteCommands<Route>, extras?: NavigationExtras): Promise<boolean>;
-    }
-}
+// declare module '@angular/router' {
+//     interface Router
+//     {
+//         navigate<
+//             Route extends
+//             & ComposableRoute<ReadonlyRoute<string, string, any>>
+//             & WithNavigationX<any, any, string>
+//         >(url: RouteCommands<Route>, extras?: NavigationExtras): Promise<boolean>;
+//     }
+// }
--- libs/angular-zen/router-x/utils/src/lib/_wrap-in-macro-task.ts
+++ libs/angular-zen/router-x/utils/src/lib/_wrap-in-macro-task.ts
@@ -1,17 +1,17 @@
-import { finalize, Observable } from 'rxjs';
+// import { finalize, Observable } from 'rxjs';
 
-// Replicated a minimal version of zone.js for half-strong-typing sake.
-declare const Zone: {
-    current: {
-        scheduleMacroTask(...args: unknown[]): { invoke(): unknown; }
-    } 
-};
+// // Replicated a minimal version of zone.js for half-strong-typing sake.
+// declare const Zone: {
+//     current: {
+//         scheduleMacroTask(...args: unknown[]): { invoke(): unknown; }
+//     } 
+// };
 
-export function wrapInMacroTask<T>(observable: Observable<T>, macroTaskNamePrefix: string): Observable<T>
-{
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    const macroTask = Zone.current.scheduleMacroTask(`${ macroTaskNamePrefix }-${ Math.random() }`, () => {}, {}, () => {}, () => {});
+// export function wrapInMacroTask<T>(observable: Observable<T>, macroTaskNamePrefix: string): Observable<T>
+// {
+//     // eslint-disable-next-line @typescript-eslint/no-empty-function
+//     const macroTask = Zone.current.scheduleMacroTask(`${ macroTaskNamePrefix }-${ Math.random() }`, () => {}, {}, () => {}, () => {});
 
-    // Signal end of macro task on completion or error and allow server to return
-    return observable.pipe(finalize(() => macroTask.invoke()));
-}
+//     // Signal end of macro task on completion or error and allow server to return
+//     return observable.pipe(finalize(() => macroTask.invoke()));
+// }
