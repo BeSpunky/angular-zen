95858132165e9555ffab7919cfed0326072feeb1
Shy Agam <us@bespunky.io>
2023-03-30 12:01:28 +0300

    🚫 Disabled strong navigate method overload export

--- libs/angular-zen/router-x/navigation/src/index.ts
+++ libs/angular-zen/router-x/navigation/src/index.ts
@@ -2,4 +2,4 @@ export * from './lib/navigation-x.injector';
 export * from './lib/navigation-x.providers';
 export * from './lib/navigation-x.route-creator';
 
-export * from './lib/types/strong-router';
+// export * from './lib/types/strong-router';
